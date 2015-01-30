4df47707555e302a7e0032083f7078d189c28890
Dennisbonke <dennisb@kabelfoon.net>
2015-01-30 23:21:04 +0100

    Fixed ArrayIndexOutOfBoundsException and added the other days. Checks for teachers that aren't great for the day.

--- src/DateTest.java
+++ src/DateTest.java
@@ -10,11 +10,17 @@ public class DateTest {
 
     public static void main (String[] args) {
 
-        checkDay();
+
+
+        checkDayForTeachers();
 
     }
 
-    public static void checkDay(){
+    public static void checkDayForLessons(){
+
+    }
+
+    public static void checkDayForTeachers(){
         String dayNames[] = new DateFormatSymbols().getWeekdays();
         Calendar date = Calendar.getInstance();
         System.out.println("Day: " + dayNames[date.get(Calendar.DAY_OF_WEEK)]);
@@ -22,8 +28,30 @@ public class DateTest {
         day = dayNames[date.get(Calendar.DAY_OF_WEEK)];
 
         System.out.println(day);
-        if (day == dayNames[date.get(Calendar.THURSDAY)]){
+        if (day == dayNames[Calendar.SUNDAY]){
+            System.out.println("No Sabbar :)!");
+            System.out.println("Weekend :)!");
+        }
+        else if (day == dayNames[Calendar.MONDAY]){
             System.out.println("Sabbar :(");
+        }
+        else if (day == dayNames[Calendar.TUESDAY]){
+            System.out.println("Sabbar :(");
+        }
+        else if (day == dayNames[Calendar.WEDNESDAY]){
+            System.out.println("No Sabbar :)!");
+        }
+        else if (day == dayNames[Calendar.THURSDAY]){
+            System.out.println("Sabbar :(");
+        }
+        else if (day == dayNames[Calendar.FRIDAY]){
+            System.out.println("No Sabbar :)!");
+            System.out.println("Wiebols :(");
+            System.out.println("Weekend :)!");
+        }
+        else if (day == dayNames[Calendar.SATURDAY]){
+            System.out.println("No Sabbar :)!");
+            System.out.println("Weekend :)!");
         }
     }
 
